8818164af5142be3e85e87efa1224820c8e56aee
Jonatan Nyberg <nyberg112@gmail.com>
2021-11-19 10:14:49 +0100

    JS docs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Inlämningsuppgift 1
 
-### Uppgiften bestod i att utveckla ett textbaserat och interaktivt spel. 
+### Uppgiften bestod i att utveckla ett textbaserat och interaktivt spel. Detta spelet är till största del utvecklat i javaScript tillsammans med HTML och CSS. Spelet utspelar sig i början på denna planten där användaren får möjlighet att resa till rymden. Beroende på svar från användaren kommer olika saker att hända i spelet.
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = main;
 
-//Global Variables
+//Globala variabler
 const gameText = document.getElementById("text");
 const gameInput = document.getElementById("input");
 const gameButton = document.getElementById("btn");
@@ -9,18 +9,24 @@ const gameButtonThree = document.getElementById("btn-three");
 const colorObjekts = ["röd", "blå", "grön"];
 
 /**
- * Funktionen aktiveras så fort sidan har laddats och 
+ * Funktion som aktiveras så fort sidan har laddats och anropar ytterligare en funktion vid namn addEventListeners
  */
 function main() {
   addEventListeners();
 }
 
-//Eventlisteners
+/**
+ * Funktion som inväntar interaktion från användare. Vid klick anropas funktionen stageOne
+ */
 function addEventListeners() {
   gameButton.onclick = stageOne; 
 }
 
-//Funktionalitet
+/**
+ * Funktion som manipulerar DOM:en genom att ändra såväl som skapa nya element. 
+ * funktionen inhämtar och visar även tidigare input användaren gjort.
+ * Funktionen har även två onclick event som anropar nya funktioner.
+ */
 function stageOne() {
   gameText.innerHTML = `${gameInput.value}, du har fått möjlighet att resa enkelväg med NASA till Mars, vad svarar du?`;
 
@@ -33,6 +39,10 @@ function stageOne() {
   gameButtonTwo.onclick = stageTwoGameOver;
 }
 
+/**
+ * Funktion som anropas om användare klickar på "NEJ". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * Funktionen har i sin tur ett onclick event som anropar en funktion som börjar om spelet i ett uppdaterat läge.
+ */
 function stageTwoGameOver () {
   gameText.innerHTML = "Du tackade nej, återgick till vardagen och levde lycklig i alla dina dagar...eller?";
 
@@ -44,6 +54,10 @@ function stageTwoGameOver () {
   }
 }
 
+/**
+ * Funktion som anropas om användare klickar på "JA". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * Funktionen har i sin tur flera onclick event som anropar olika funktioner beroende på användarens val.
+ */
 function stageTwo() {
   gameText.innerHTML = "2 år senare... Mars är inom räckhåll när ett oidentifierat objekt plötsligt dyker upp från ingenstans. vad gör du nu?"
   
@@ -57,6 +71,10 @@ function stageTwo() {
   gameButtonThree.onclick = stageThreeHonk;
 }
 
+/**
+ * Funktion som anropas om användare klickar på "skjut rymmdmissiler". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * Funktionen har i sin tur ett onclick event som anropar en funktion som börjar om spelet i ett uppdaterat läge.
+ */
 function stageThreeGameOver() {
   gameText.innerHTML = "Din handling påverkade inte objektet men gick inte förbi obemärkt. Det oidentifierade objektet vecklar ut vad som ser ut som en gigantisk metallbanan och skjuter iväg en laserstråle som upplöser dig och ditt skepp till atomer...Aj"
 
@@ -69,6 +87,10 @@ function stageThreeGameOver() {
   }
 }
 
+/**
+ * Funktion som anropas om användare klickar på "Landa på objektet". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * Funktionen har i sin tur flera onclick event som anropar specifika funktioner beroende på vilken knapp som klickas.
+ */
 function stageThreeLand() {
   gameText.innerHTML = "Du landar skeppet på objektet och genomför en rymdpromenad för att ta lite prover. Väl ute på objektet dyker en varelse upp framför dig. vad gör du?"
 
@@ -80,6 +102,11 @@ function stageThreeLand() {
   gameButtonTwo.onclick = stageFour;
 }
 
+/**
+ * Funktion som anropas om användare klickar på "Manöverera skeppet". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * fuktionen har även ett inputfält där användaren ska mata in valfritt meddelande.
+ * Funktionen har i sin tur ett onclick event som anropar en funktion för att ta sig vidare.
+ */
 function stageThreeHonk() {
   gameText.innerHTML = "Du manövererar skeppet för att inte kollidera, men det visar sig att objektet ändrar sin position så att du inte kan ta dig förbi. Intressant tänker du och försöker ta kontakt med objektet genom att sända ut ett meddelande. Vad skriver du?"
 
@@ -92,6 +119,11 @@ function stageThreeHonk() {
   gameButton.onclick = stageThreeHonkNext;
 }
 
+/**
+ * Funktion som anropas om användare klickar på "sänd meddelande". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * funktionen tar även in tidigare input och visar detta för användaren.
+ * Funktionen har flera onclick event som anropar specifika funktioner beroende på vilken knapp som klickas.
+ */
 function stageThreeHonkNext() {
   gameText.innerHTML = `Ditt meddelande "${gameInput.value}" verkar ha mottagits eftersom skeppet automatiskt styrs ner på objektet. Du tar på din rymddräckt och går ut där en varelse står och väntar på dig. Vad gör du nu?`
 
@@ -106,6 +138,10 @@ function stageThreeHonkNext() {
 
 }
 
+/**
+ * Funktion som anropas om användare klickar på "Skjuter varelsen". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * Funktionen har ett onclick event som anropar en funktion som börjar om spelet i ett uppdaterat läge.
+ */
 function stageFourGameOver() {
   gameText.innerHTML = "Varelsen verkar ha någon form av sköld runt sig eftersom skottet bara stutsade iväg. Varlsen suckar, tar fram sitt vapen och skjuter dig... du fallet ihop och slocknar till ...ajajajaj"
 
@@ -118,6 +154,12 @@ function stageFourGameOver() {
   }
 }
 
+/**
+ * Funktion som anropas om användare klickar på "Hälsar på varelsen". Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ * Funktionen har ett input fält där användaren matar in sitt svar. När användaren rör musen över knappen så anropas en funktion som loopar igenom den globala arrayen.
+ * Beroende på input aktiveras olika funktioner genom ett if else statement.
+ * När användaren därefter klickar på välj-knappen anropas en specifik funktion.
+ */
 function stageFour() {
   gameText.innerHTML = "Det visar sig att varelsen förstår ditt språk och tar fram tre liknande objekt: en dosa med en röd, en blå och en grön knapp, Sedan säger varlsen välj en färg. Vilken färg väljer du?"
 
@@ -126,6 +168,7 @@ function stageFour() {
   gameButton.innerHTML = "Välj";
   gameButtonTwo.style.display = "none";
   gameButtonThree.style.display = "none";
+
   gameButton.onmouseenter = function() {
     for (let i = 0; i < colorObjekts.length; i++) {
       if (gameInput.value === "röd") {
@@ -144,6 +187,9 @@ function stageFour() {
     }
 }
 
+/**
+ * Funktion som anropas om användare skriver "röd" i input-fältet. Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ */
 function stageFiveRed() {
   gameText.innerHTML = "Du väljer den röda dosan. Varelsen ger dig den och knäpper sedan med fingret...Du vaknar upp i ditt skepp och insåg att allt var en dröm tills du ser dosan i sätet bredvid dig. Du tar upp dosan, kollar lite på den och trycker sedan på den röda knappen...BOOM! du och hela skeppet sprängs i tusen bitar...ajajajaj...THE END"
 
@@ -151,6 +197,9 @@ function stageFiveRed() {
   gameButton.style.display = "none";
 }
 
+/**
+ * Funktion som anropas om användare skriver "blå" i input-fältet. Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ */
 function stageFiveBlue() {
   gameText.innerHTML = "Du väljer den blåa dosan. Varelsen ger dig den och knäpper sedan med fingret...Du vaknar upp i ditt skepp och insåg att allt var en dröm tills du ser dosan i sätet bredvid dig. Du tar upp dosan, kollar lite på den och trycker sedan på den blåa knappen...jaha tänker du, vilket skämt och åker vidare mot Mars... THE END"
   
@@ -158,6 +207,9 @@ function stageFiveBlue() {
   gameButton.style.display = "none";
 }
 
+/**
+ * Funktion som anropas om användare skriver "grön" i input-fältet. Precis som tidigare funktionen manipulerar denna funktion element i DOM:en
+ */
 function stageFiveGreen() {
   gameText.innerHTML = "Du väljer den röda dosan. Varelsen ger dig den och knäpper sedan med fingret...Du vaknar upp i ditt skepp och insåg att allt var en dröm tills du ser dosan i sätet bredvid dig. Du tar upp dosan, kollar lite på den och trycker sedan på den gröna knappen..."
 
